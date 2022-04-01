0fe834d54ef9d6b41877584121d1cfa0c3da2ebb
Balakumar Subramani <balakumartceit@gmail.com>
2022-04-01 11:14:18 +0530

    Remove vaadin-impl from main

--- webui/src/main/java/com/balakumar/pm/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.balakumar.pm.utils;
-
-public class Constants {
-    public static final String PORTFOLIO = "Portfolio";
-    public static final String CREATE = "Create";
-    public static final String SUBMIT = "Submit";
-    public static final String DELETE = "Delete";
-}
